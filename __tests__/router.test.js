5d991d8d15e50ebab56d74f7c3869ca33c0121c8
anm004 <anm004@ucsd.edu>
2021-05-23 20:12:20 -0700

    part1 finished

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,24 @@
 /**
  * @jest-environment jsdom
  */
+ import { pushToHistory } from '../scripts/router.js';
 
+ describe('push to history', () => {
+    //test settings case
+    test('settings case', () => {
+      expect(pushToHistory('settings', 1).state.page).toBe('settings');
+      expect(window.history.length).toBe(2);
+    });
+    //test entry case
+    test('entry case', () => {
+        expect(pushToHistory('entry', 2).state.page).toBe('entry2');
+        expect(window.history.length).toBe(3);
+      
+    });
+    //test defualt case
+    test('default case', () => {
+        expect(pushToHistory('',1).state.page).toBe(undefined);
+        expect(window.history.length).toBe(4);
+        
+      });
+  });
